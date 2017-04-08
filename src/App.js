5f081917493bc99d589f3964987666c4f483d0ec
Thibaut Tiberghien <thibaut@planecq.com>
2017-04-08 11:01:55 +0800

    eslint config and fix

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import Faux from 'react-faux-dom';
-import * as d3 from 'd3';
-import bumpLayer from './bumpLayer';
+import React from 'react'
+import Faux from 'react-faux-dom'
+import * as d3 from 'd3'
+import bumpLayer from './bumpLayer'
 
 const App = React.createClass({
   mixins: [Faux.mixins.core, Faux.mixins.anim],
-  getInitialState() {
-    return { look: 'stacked' };
+  getInitialState () {
+    return { look: 'stacked' }
   },
-  render() {
+  render () {
     return (
       <div>
         <button onClick={this.toggle}>Toggle</button>
         {this.state.chart}
       </div>
-    );
+    )
   },
-  toggle() {
+  toggle () {
     if (this.state.look === 'stacked') {
-      this.setState({ look: 'grouped' });
-      this.transitionGrouped();
+      this.setState({ look: 'grouped' })
+      this.transitionGrouped()
     } else {
-      this.setState({ look: 'stacked' });
-      this.transitionStacked();
+      this.setState({ look: 'stacked' })
+      this.transitionStacked()
     }
   },
-  componentDidMount() {
+  componentDidMount () {
     // This will create a faux div and store its virtual DOM
     // in state.chart
-    var faux = this.connectFauxDOM('div', 'chart');
-    var component = this;
+    var faux = this.connectFauxDOM('div', 'chart')
+    var component = this
 
     // see https://bl.ocks.org/mbostock/3943967 + changes:
     // - feeding D3 the faux node created above
@@ -37,23 +37,23 @@ const App = React.createClass({
     // - attaching the radio button callbacks to the component
     // - deleting the radio button (as we do the toggling through the react button)
 
-    var n = 4; // number of layers
-    var m = 58; // number of samples per layer
-    var stack = d3.layout.stack();
-    var layers = stack(d3.range(n).map(() => bumpLayer(m, 0.1)));
+    var n = 4 // number of layers
+    var m = 58 // number of samples per layer
+    var stack = d3.layout.stack()
+    var layers = stack(d3.range(n).map(() => bumpLayer(m, 0.1)))
     var yGroupMax = d3.max(layers, layer => {
-      return d3.max(layer, d => d.y);
-    });
+      return d3.max(layer, d => d.y)
+    })
     var yStackMax = d3.max(layers, layer => {
-      return d3.max(layer, d => d.y0 + d.y);
-    });
-    var margin = { top: 40, right: 10, bottom: 20, left: 10 };
-    var width = 960 - margin.left - margin.right;
-    var height = 500 - margin.top - margin.bottom;
-    var x = d3.scale.ordinal().domain(d3.range(m)).rangeRoundBands([0, width], 0.08);
-    var y = d3.scale.linear().domain([0, yStackMax]).range([height, 0]);
-    var color = d3.scale.linear().domain([0, n - 1]).range(['#aad', '#556']);
-    var xAxis = d3.svg.axis().scale(x).tickSize(0).tickPadding(6).orient('bottom');
+      return d3.max(layer, d => d.y0 + d.y)
+    })
+    var margin = { top: 40, right: 10, bottom: 20, left: 10 }
+    var width = 960 - margin.left - margin.right
+    var height = 500 - margin.top - margin.bottom
+    var x = d3.scale.ordinal().domain(d3.range(m)).rangeRoundBands([0, width], 0.08)
+    var y = d3.scale.linear().domain([0, yStackMax]).range([height, 0])
+    var color = d3.scale.linear().domain([0, n - 1]).range(['#aad', '#556'])
+    var xAxis = d3.svg.axis().scale(x).tickSize(0).tickPadding(6).orient('bottom')
 
     var svg = d3
       .select(faux)
@@ -61,7 +61,7 @@ const App = React.createClass({
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
       .append('g')
-      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
     var layer = svg
       .selectAll('.layer')
@@ -69,7 +69,7 @@ const App = React.createClass({
       .enter()
       .append('g')
       .attr('class', 'layer')
-      .style('fill', (d, i) => color(i));
+      .style('fill', (d, i) => color(i))
 
     var rect = layer
       .selectAll('rect')
@@ -79,20 +79,20 @@ const App = React.createClass({
       .attr('x', d => x(d.x))
       .attr('y', height)
       .attr('width', x.rangeBand())
-      .attr('height', 0);
+      .attr('height', 0)
 
     rect
       .transition()
       .delay((d, i) => i * 10)
       .attr('y', d => y(d.y0 + d.y))
-      .attr('height', d => y(d.y0) - y(d.y0 + d.y));
+      .attr('height', d => y(d.y0) - y(d.y0 + d.y))
 
-    this.animateFauxDOM(800);
+    this.animateFauxDOM(800)
 
-    svg.append('g').attr('class', 'x axis').attr('transform', 'translate(0,' + height + ')').call(xAxis);
+    svg.append('g').attr('class', 'x axis').attr('transform', 'translate(0,' + height + ')').call(xAxis)
 
     this.transitionGrouped = () => {
-      y.domain([0, yGroupMax]);
+      y.domain([0, yGroupMax])
 
       rect
         .transition()
@@ -102,27 +102,27 @@ const App = React.createClass({
         .attr('width', x.rangeBand() / n)
         .transition()
         .attr('y', d => y(d.y))
-        .attr('height', d => height - y(d.y));
+        .attr('height', d => height - y(d.y))
 
-      component.animateFauxDOM(2000);
+      component.animateFauxDOM(2000)
     };
 
     this.transitionStacked = () => {
-      y.domain([0, yStackMax]);
+      y.domain([0, yStackMax])
 
       rect
         .transition()
         .duration(500)
-        //.delay((d, i) => i * 10)
+        // .delay((d, i) => i * 10)
         .attr('y', d => y(d.y0 + d.y))
         .attr('height', d => y(d.y0) - y(d.y0 + d.y))
         .transition()
         .attr('x', d => x(d.x))
-        .attr('width', x.rangeBand());
+        .attr('width', x.rangeBand())
 
-      component.animateFauxDOM(2000);
+      component.animateFauxDOM(2000)
     };
   }
-});
+})
 
-export default App;
+export default App
--- src/bumpLayer.js
+++ src/bumpLayer.js
@@ -1,26 +1,26 @@
 // Inspired by Lee Byron's test data generator.
-var bumpLayer = (n, o) => {
+function bumpLayer (n, o) {
   var bump = a => {
-    var x = 1 / (0.1 + Math.random());
-    var y = 2 * Math.random() - 0.5;
-    var z = 10 / (0.1 + Math.random());
+    var x = 1 / (0.1 + Math.random())
+    var y = 2 * Math.random() - 0.5
+    var z = 10 / (0.1 + Math.random())
     for (var i = 0; i < n; i++) {
-      var w = (i / n - y) * z;
-      a[i] += x * Math.exp(-w * w);
+      var w = (i / n - y) * z
+      a[i] += x * Math.exp(-w * w)
     }
-  };
+  }
 
-  var a = [];
-  var i;
+  var a = []
+  var i
   for (i = 0; i < n; ++i) {
-    a[i] = o + o * Math.random();
+    a[i] = o + o * Math.random()
   }
   for (i = 0; i < 5; ++i) {
-    bump(a);
+    bump(a)
   }
   return a.map((d, i) => {
-    return { x: i, y: Math.max(0, d) };
-  });
-};
+    return { x: i, y: Math.max(0, d) }
+  })
+}
 
-export default bumpLayer;
+export default bumpLayer
