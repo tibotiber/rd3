b658e000b7ea0987162985e79394d3b80f6ad255
Thibaut Tiberghien <thibaut@planecq.com>
2017-04-09 23:31:24 +0800

    Use D3 updates on re-render

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -24,11 +24,11 @@ const BarChart = React.createClass({
     }
   },
   componentDidMount () {
-    this.renderD3()
+    this.renderD3(true)
   },
   componentDidUpdate (prevProps, prevState) {
     if (this.props !== prevProps) {
-      this.renderD3()
+      this.renderD3(false)
     }
   },
   render () {
@@ -48,54 +48,40 @@ const BarChart = React.createClass({
       this.transitionStacked()
     }
   },
-  renderD3 () {
+  renderD3 (firstRender) {
+    const n = this.props.data.length // number of layers
+    const stack = d3.layout.stack()
+    const layers = stack(this.props.data)
+    const yGroupMax = d3.max(layers, layer => d3.max(layer, d => d.y))
+    const yStackMax = d3.max(layers, layer => d3.max(layer, d => d.y0 + d.y))
+    const margin = { top: 20, right: 10, bottom: 50, left: 50 }
+    const width = this.props.width - margin.left - margin.right
+    const height = this.props.height - margin.top - margin.bottom
+    const x = d3.scale.ordinal().domain(this.props.xDomain).rangeRoundBands([0, width], 0.08)
+    const y = d3.scale.linear().domain([0, yStackMax]).range([height, 0])
+    const color = d3.scale.linear().domain([0, n - 1]).range(this.props.colors)
+    const xAxis = d3.svg.axis().scale(x).orient('bottom')
+    const yAxis = d3.svg.axis().scale(y).orient('left')
+
     // create a faux div and store its virtual DOM in state.chart
-    var faux = this.connectFauxDOM('div', 'chart')
-    var n = this.props.data.length // number of layers
-    var stack = d3.layout.stack()
-    var layers = stack(this.props.data)
-    var yGroupMax = d3.max(layers, layer => {
-      return d3.max(layer, d => d.y)
-    })
-    var yStackMax = d3.max(layers, layer => {
-      return d3.max(layer, d => d.y0 + d.y)
-    })
-    var margin = { top: 20, right: 10, bottom: 50, left: 50 }
-    var width = this.props.width - margin.left - margin.right
-    var height = this.props.height - margin.top - margin.bottom
-    var x = d3.scale.ordinal().domain(this.props.xDomain).rangeRoundBands([0, width], 0.08)
-    var y = d3.scale.linear().domain([0, yStackMax]).range([height, 0])
-    var color = d3.scale.linear().domain([0, n - 1]).range(this.props.colors)
-    var xAxis = d3.svg.axis().scale(x).orient('bottom')
-    var yAxis = d3.svg.axis().scale(y).orient('left')
+    let faux = this.connectFauxDOM('div', 'chart')
 
-    var svg = d3
-      .select(faux)
-      .append('svg')
-      .attr('class', this.props.className)
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+    let svg = firstRender
+      ? d3
+          .select(faux)
+          .append('svg')
+          .attr('class', this.props.className)
+          .attr('width', width + margin.left + margin.right)
+          .attr('height', height + margin.top + margin.bottom)
+          .append('g')
+          .attr('transform', `translate(${margin.left}, ${margin.top})`)
+      : d3.select(faux).select('svg').select('g')
 
-    var layer = svg
-      .selectAll('.layer')
-      .data(layers)
-      .enter()
-      .append('g')
-      .attr('class', 'layer')
-      .style('fill', (d, i) => color(i))
+    let layer = svg.selectAll('.layer').data(layers)
+    layer.enter().append('g').attr('class', 'layer').style('fill', (d, i) => color(i))
 
-    var rect = layer
-      .selectAll('rect')
-      .data(d => d)
-      .enter()
-      .append('rect')
-      .attr('x', d => x(d.x))
-      .attr('y', height)
-      .attr('width', x.rangeBand())
-      .attr('height', 0)
-
+    let rect = layer.selectAll('rect').data(d => d)
+    rect.enter().append('rect').attr('x', d => x(d.x)).attr('y', height).attr('width', x.rangeBand()).attr('height', 0)
     rect
       .transition()
       .delay((d, i) => i * 10)
@@ -103,22 +89,27 @@ const BarChart = React.createClass({
       .attr('height', d => y(d.y0) - y(d.y0 + d.y))
     this.animateFauxDOM(800)
 
-    svg.append('g').attr('class', 'x axis').attr('transform', `translate(0, ${height})`).call(xAxis)
-    svg
-      .append('text')
-      .attr('transform', `translate(${width / 2} ,${height + margin.bottom - 5})`)
-      .style('text-anchor', 'middle')
-      .text(this.props.xLabel)
+    if (firstRender) {
+      svg.append('g').attr('class', 'x axis').attr('transform', `translate(0, ${height})`).call(xAxis)
+      svg
+        .append('text')
+        .attr('transform', `translate(${width / 2} ,${height + margin.bottom - 5})`)
+        .style('text-anchor', 'middle')
+        .text(this.props.xLabel)
 
-    svg.append('g').attr('class', 'y axis').attr('transform', 'translate(0, 0)').call(yAxis)
-    svg
-      .append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', 0 - margin.left)
-      .attr('x', 0 - height / 2)
-      .attr('dy', '1em')
-      .style('text-anchor', 'middle')
-      .text(this.props.yLabel)
+      svg.append('g').attr('class', 'y axis').attr('transform', 'translate(0, 0)').call(yAxis)
+      svg
+        .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 0 - margin.left)
+        .attr('x', 0 - height / 2)
+        .attr('dy', '1em')
+        .style('text-anchor', 'middle')
+        .text(this.props.yLabel)
+    } else {
+      svg.select('g.x.axis').call(xAxis)
+      svg.select('g.y.axis').call(yAxis)
+    }
 
     this.transitionGrouped = () => {
       y.domain([0, yGroupMax])
